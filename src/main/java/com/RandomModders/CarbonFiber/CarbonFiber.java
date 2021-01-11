6e02fd32807cfa527f27f43dfb63b2df58ee781a
nighthunter56 <68067532+nighthunter56@users.noreply.github.com>
2021-01-11 14:22:01 +0400

    Carbon Blocks Added
    I Added Carbon Blocks But Not The Texture

--- src/main/java/com/RandomModders/CarbonFiber/CarbonFiber.java
+++ src/main/java/com/RandomModders/CarbonFiber/CarbonFiber.java
@@ -2,6 +2,10 @@ package com.RandomModders.CarbonFiber;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -11,13 +15,15 @@ public class CarbonFiber implements ModInitializer {
     //Items
     public static final Item CARBON_INGOT = new Item(new FabricItemSettings().group(ItemGroup.MISC));
     //Blocks
+    public static final Block CARBON_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).hardness(4.0f));
 
     @Override
     public void onInitialize() {
         //ItemsRegister
         Registry.register(Registry.ITEM, new Identifier("carbon", "carbon_ingot"), CARBON_INGOT);
         //BlocksRegister
-
+        Registry.register(Registry.BLOCK, new Identifier("carbon", "carbon_block"), CARBON_BLOCK);
         //BlockItemRegisters
+        Registry.register(Registry.ITEM, new Identifier("tutorial", "carbon_block"), new BlockItem(CARBON_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
     }
 }
