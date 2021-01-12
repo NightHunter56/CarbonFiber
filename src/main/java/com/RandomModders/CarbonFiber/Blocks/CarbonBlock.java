aee42c59dd800e99cd70bdfd8ac9c5973fde7dfe
nighthunter56 <68067532+nighthunter56@users.noreply.github.com>
2021-01-12 13:48:42 +0400

    Changed Hardness Value
    everything in title

--- src/main/java/com/RandomModders/CarbonFiber/Blocks/CarbonBlock.java
+++ src/main/java/com/RandomModders/CarbonFiber/Blocks/CarbonBlock.java
@@ -10,7 +10,7 @@ public class CarbonBlock extends Block {
 
     public CarbonBlock() {
         super(FabricBlockSettings.of(Material.METAL)
-                .hardness(4.0f)
+                .hardness(3.0f)
                 .requiresTool()
                 .breakByTool(FabricToolTags.PICKAXES,4));
     }
