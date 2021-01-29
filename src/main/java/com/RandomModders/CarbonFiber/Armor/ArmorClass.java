052a825a7ebb26a6bb7feb224c33898842ed284f
nighthunter56 <68067532+nighthunter56@users.noreply.github.com>
2021-01-29 13:22:35 +0400

    Updated Armor Values

--- src/main/java/com/RandomModders/CarbonFiber/Armor/ArmorClass.java
+++ src/main/java/com/RandomModders/CarbonFiber/Armor/ArmorClass.java
@@ -6,45 +6,46 @@ import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
 
 public class ArmorClass implements ArmorMaterial {
-
+    private static final int[] BASE_DURABILITY = new int[] {13, 15, 16, 11};
+    private static final int[] PROTECTION_VALUES = new int[] {13, 15, 16, 11};
 
     @Override
     public int getDurability(EquipmentSlot slot) {
-        return 0;
+        return BASE_DURABILITY[slot.getEntitySlotId()] * X;
     }
 
     @Override
     public int getProtectionAmount(EquipmentSlot slot) {
-        return 0;
+        return PROTECTION_VALUES[slot.getEntitySlotId()];
     }
 
     @Override
     public int getEnchantability() {
-        return 0;
+        return X;
     }
 
     @Override
     public SoundEvent getEquipSound() {
-        return null;
+        return SoundEvents.ITEM_ARMOR_EQUIP_X;
     }
 
     @Override
     public Ingredient getRepairIngredient() {
-        return null;
+        return Ingredient.ofItems(RegisterItems.X);
     }
 
     @Override
     public String getName() {
-        return null;
+        return "name";
     }
 
     @Override
     public float getToughness() {
-        return 0;
+        return X.0F;
     }
 
     @Override
     public float getKnockbackResistance() {
-        return 0;
+        return 0.XF;
     }
 }
