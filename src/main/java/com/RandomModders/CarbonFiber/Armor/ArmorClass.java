52fe4fea9a38e0296b611491ec5fcc08e8585ffc
nighthunter56 <68067532+nighthunter56@users.noreply.github.com>
2021-01-29 13:19:55 +0400

    Added Armor Base Values

--- src/main/java/com/RandomModders/CarbonFiber/Armor/ArmorClass.java
+++ src/main/java/com/RandomModders/CarbonFiber/Armor/ArmorClass.java
@@ -1,4 +1,50 @@
 package com.RandomModders.CarbonFiber.Armor;
 
-public class ArmorClass {
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+
+public class ArmorClass implements ArmorMaterial {
+
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return 0;
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return 0;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 0;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return null;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public float getToughness() {
+        return 0;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0;
+    }
 }
